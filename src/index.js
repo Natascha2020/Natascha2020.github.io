ffe9d7f7d5402f6169a5234687526ceed780bd9d
Natascha Hof <NataschaHof@gmx.de>
2020-07-23 21:32:58 +0200

    Added JavaScript

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function Winner(){
+    document.getElementById("WONDA").style.visibility="visible"};
+
+function WinnerTwo(){
+    document.getElementById("MISHTOTHEWORLD").style.visibility="visible"};
+
+function WinnerThree(){
+    document.getElementById("Statttour").style.visibility="visible"};
